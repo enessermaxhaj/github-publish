77283210b16834faf99ad02ddcd1aaeb418f75d2
ubuntue <enessermaxhaj1992@gmailcom>
2026-02-26 14:23:46 +0100

    skii

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Docker + GitHub Actions!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
